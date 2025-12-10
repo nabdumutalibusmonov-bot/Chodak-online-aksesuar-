f5ae9a45ae13dc15cc07429fd3a2251ad5354f71
TechnoPul <technopul@MacBook-Pro.local>
2025-12-10 13:50:19 +0500

    push

---
+++ src/constants/db.js
@@ -0,0 +1,81 @@
+export function getData() {
+	return [
+		{
+			title: 'Erkaklar uchun soat',
+			price: 11,
+			Image:
+				'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqR9i20ptOa3pjeLwLUi6kSVpLjioYiXgQbQ&s',
+			id: 1,
+		},
+		{
+			title: 'premium barsetka',
+			price: 20,
+			Image:
+				'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-ibc2KLL0fjCv-e-o-U8Ec9zlmLownjsVFQ&s',
+			id: 2,
+		},
+		{
+			title: 'Air pods Pro',
+			price: 13.5,
+			Image:
+				'https://img.freepik.com/free-photo/rendering-smart-home-device_23-2151039320.jpg?semt=ais_incoming&w=740&q=80',
+			id: 3,
+		},
+		{
+			title: 'mers deflektr',
+			price: 12,
+			Image:
+				'https://olcha.uz/image/original/category/VasWe4boWvAg1agTYMzm9oh5SwRyrYcO8PMjks2Tl7n4RhICTS8eoWzm8yVI.png',
+			id: 4,
+		},
+		{
+			title: 'Camera-server-kalonka',
+			price: 21,
+			Image:
+				'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQv_-hzD_NNYA90Iv2UQVKOWFDRVhOiag32NQ&s',
+			id: 5,
+		},
+		{
+			title: 'Aksesuar',
+			price: 9,
+			Image:
+				'https://i.pinimg.com/736x/76/57/09/765709f2cd8d3ee12b63432f4f99ac21.jpg',
+			id: 6,
+		},
+		{
+			title: 'Royale achki',
+			price: 33,
+			Image:
+				'https://www.shutterstock.com/image-photo/black-smart-eye-glasses-260nw-2496683307.jpg',
+			id: 7,
+		},
+		{
+			title: 'Ustalar uchun nabor',
+			price: 28.99,
+			Image:
+				'https://optoweek.com.ua/image/cache/catalog/products/procraft/zatochka/nabor-aksesuarov-dlja-gravera-procraft-gk418-1000x1000.jpg',
+			id: 8,
+		},
+        {
+			title: 'Kakusika sumka',
+			price: 13,
+			Image:
+				'https://aspor.ua/image/cache/catalog/aspor/17412/portativniy-organayzer-kakusiga-ksc-1576-dlya-kabel-v-gadzhet-v-vodonepronikniy-chohol-dlya-zber-gannya-tsifrovih-aksesuar-v-chorniy-1-700x700.jpg',
+			id: 7,
+		},
+        {
+			title: 'Smart watchlar uchun gilof',
+			price: 8,
+			Image:
+				'https://m.media-amazon.com/images/I/71RTXKVdmgL.jpg',
+			id: 7,
+		},
+        {
+			title: 'Go Pro Hero',
+			price: 23,
+			Image:
+				'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5rwhc6FzrlB0w2NdKp4xupDh8wCt_pyrrFw&s',
+			id: 7,
+		},
+	];
+}
